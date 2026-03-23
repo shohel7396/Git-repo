9dc9f2d3a7f0db454c38a247903faa93a257bd63
shohel7396 <sunarshohel7396@gmail.com>
2026-03-23 12:27:36 +0530

    Correct syntax and formatting in app.py
    Fixed syntax errors and formatting issues in app.py.

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-# app.py
-from flask import Flask
-app = Flask(__name__)
+
+from flask import Flaskapp = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Hello Jerry DevOps Project!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80):w
+    app.run(host="0.0.0.0", port=80)
